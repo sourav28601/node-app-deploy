b53b84b17aec2744f472db14c2dcf558bb426771
sourav28601 <souravrajput7975@gmail.com>
2026-05-15 00:21:26 +0530

    script issue fix2

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Node App Running on AWS EC2 !!!!!!!!!....................party toh bnti h bro!!!! dance party krenge!");
+  res.send("Node App Running on AWS EC2 !!!!!!!!!....................party toh bnti h bro!!!! dance party krenge");
 });
 
 app.listen(3000, () => {
