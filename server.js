ee2195051f9b2c6d2aec561078b8eaebec393153
sourav28601 <souravrajput7975@gmail.com>
2026-05-14 22:48:55 +0530

    modified server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Node App Running on AWS EC2 !!!!!!!!!....................party toh bnti h");
+  res.send("Node App Running on AWS EC2 !!!!!!!!!....................party toh bnti h bro");
 });
 
 app.listen(3000, () => {
